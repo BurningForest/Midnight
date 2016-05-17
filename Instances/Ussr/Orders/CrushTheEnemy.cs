0fb85348fe3c5e8da1b34791b1ce0ee3dcb48f99
BurningForest <burningintheforest@gmail.com>
2016-05-17 17:13:30 +0300

    CrushTheEnemy implemented

---
+++ Instances/Ussr/Orders/CrushTheEnemy.cs
@@ -0,0 +1,51 @@
+﻿using Midnight.Abilities.Activating;
+using Midnight.ActionManager;
+using Midnight.Actions;
+using Midnight.Cards;
+using Midnight.Cards.Enums;
+using Midnight.Cards.Types;
+
+namespace Midnight.Instances.Ussr.Orders
+{
+	public class CrushTheEnemy : Order
+	{
+		// Нанесите 1 повреждение выбранному штабу или технике
+
+		public static readonly Proto proto = new Proto() {
+			id = "so_takbilotakbudet",
+			level = 1,
+			type = Type.order,
+			subtype = Subtype.order,
+			country = Country.ussr,
+
+			cost = 0,
+		};
+
+		public class CrushTheEnemyAbility : SpecificAbility
+		{
+			protected override GameAction[] Actions (FieldCard target)
+			{
+				return new[] { new DealDamage(1, card, target) };
+			}
+
+			protected override Search Targets (Search search)
+			{
+				return search
+					.Enemy().Forefront()
+					.Vehicle().Hq();
+			}
+		}
+
+		public override Proto GetProto ()
+		{
+			return proto;
+		}
+
+		public override void InitAbilities ()
+		{
+			base.InitAbilities();
+
+			abilities.Add(new CrushTheEnemyAbility());
+		}
+	}
+}
---
+++ Tests/Instances/Orders/CrushTheEnemyTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Midnight.Core;
+using Midnight.Instances.Ussr.Orders;
+using Midnight.Tests.TestInstances;
+using Midnight.Utils;
+
+namespace Midnight.Tests.Instances.Orders
+{
+	[TestClass]
+	public class CrushTheEnemyTest
+	{
+
+		[TestMethod]
+		public void CrushTheEnemy ()
+		{
+			Engine engine = new Engine();
+			Logger logger = new Logger(engine);
+			Manage manage = new Manage(engine);
+
+			var field = engine.field;
+			var player = engine.chiefs[0];
+			var enemy = engine.chiefs[1];
+
+			var crush1 = player.cards.factory.Create<CrushTheEnemy>();
+			var crush2 = player.cards.factory.Create<CrushTheEnemy>();
+
+			var hq = enemy.cards.factory.CreateDefaultHq<HqStrike>();
+			var tm = enemy.cards.factory.Create<TankMedium>();
+
+			manage.Position(tm, field.GetCell(2, 2));
+			manage.Draw(player, 2);
+
+			manage.StartGame(player);
+
+			Assert.IsTrue(manage.Order(crush1, hq).IsValid());
+			Assert.IsTrue(manage.Order(crush2, tm).IsValid());
+			Assert.AreEqual(1, hq.GetDamage());
+			Assert.AreEqual(1, tm.GetDamage());
+		}
+
+	}
+}
