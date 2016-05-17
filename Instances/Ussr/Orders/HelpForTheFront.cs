63acedba37e50c88c39e8d0250bd6de7a37323d3
BurningForest <burningintheforest@gmail.com>
2016-05-17 17:20:37 +0300

    HelpForTheFront implemented

---
+++ Instances/Ussr/Orders/HelpForTheFront.cs
@@ -0,0 +1,53 @@
+﻿using Midnight.Abilities.Activating;
+using Midnight.ActionManager;
+using Midnight.Actions;
+using Midnight.Cards;
+using Midnight.Cards.Enums;
+using Midnight.Cards.Types;
+
+namespace Midnight.Instances.Ussr.Orders
+{
+	public class HelpForTheFront : Order
+	{
+		// Возьмите карту.
+		// Восстановите 2 прочности своему штабу.
+
+		public static readonly Proto proto = new Proto() {
+			id = "so_budbditelnym",
+			level = 1,
+			type = Type.order,
+			subtype = Subtype.order,
+			country = Country.ussr,
+
+			cost = 2,
+		};
+
+		public class HelpForTheFrontAbility : SpecificAbility
+		{
+			protected override GameAction[] Actions (FieldCard target)
+			{
+				return new GameAction[] {
+					new DrawRandom(chief),
+					new HealDamage(2, card, chief.cards.GetHq())
+				};
+			}
+
+			protected override Search Targets (Search search)
+			{
+				return null;
+			}
+		}
+
+		public override Proto GetProto ()
+		{
+			return proto;
+		}
+
+		public override void InitAbilities ()
+		{
+			base.InitAbilities();
+
+			abilities.Add(new HelpForTheFrontAbility());
+		}
+	}
+}
---
+++ Tests/Instances/Orders/HelpForTheFrontTest.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Midnight.Core;
+using Midnight.Instances.Ussr.Orders;
+using Midnight.Tests.TestInstances;
+using Midnight.Utils;
+
+namespace Midnight.Tests.Instances.Orders
+{
+	[TestClass]
+	public class HelpForTheFrontTest
+	{
+
+		[TestMethod]
+		public void HelpForTheFront ()
+		{
+			Engine engine = new Engine();
+			Logger logger = new Logger(engine);
+			Manage manage = new Manage(engine);
+
+			var field = engine.field;
+			var player = engine.chiefs[0];
+
+			var hq = player.cards.factory.CreateDefaultHq<HqStrike>();
+			var help = player.cards.factory.Create<HelpForTheFront>();
+			var tank = player.cards.factory.Create<TankMedium>();
+
+			manage.Draw(help);
+			manage.Damage(5, hq, hq);
+			manage.SetResources(player, 10);
+
+			manage.StartGame(player);
+
+			Assert.AreEqual(5, hq.GetDamage());
+			Assert.IsTrue(tank.GetLocation().IsDeck());
+
+			Assert.IsTrue(manage.Order(help).IsValid());
+
+			Assert.IsTrue(tank.GetLocation().IsReserve());
+			Assert.AreEqual(3, hq.GetDamage());
+		}
+
+	}
+}
