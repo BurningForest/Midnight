78ead4fbc635a22dbfd9e88a5e1b38636aa66203
BurningForest <burningintheforest@gmail.com>
2016-05-19 06:21:34 +0300

    Correct HQs

---
+++ Instances/Ussr/Hqs/TrainingBase.cs
@@ -0,0 +1,25 @@
+﻿using Midnight.Cards.Enums;
+using Midnight.Cards.Types;
+
+namespace Midnight.Instances.Ussr.Hqs
+{
+	public class TrainingBase : Hq
+	{
+		public static readonly Proto proto = new Proto() {
+			id = "sh_uchebnayachast",
+			level = 1,
+			type = Type.hq,
+			subtype = Subtype.beginner,
+			country = Country.ussr,
+
+			power = 2,
+			toughness = 18,
+			increase = 5,
+		};
+
+		public override Proto GetProto ()
+		{
+			return proto;
+		}
+	}
+}
