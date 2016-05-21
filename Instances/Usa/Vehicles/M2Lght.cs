afa3df77582010235df5b068246f18e54355c5dd
BurningForest <burningintheforest@gmail.com>
2016-05-21 20:44:15 +0300

    M2Light card is implemented (UsaCamouflage ability)

---
+++ Instances/Usa/Vehicles/M2Lght.cs
@@ -0,0 +1,43 @@
+﻿using Midnight.Abilities.Passive;
+using Midnight.Cards.Enums;
+using Midnight.Cards.Vehicles;
+
+namespace Midnight.Instances.Usa.Vehicles
+{
+	public class M2Light : LightVehicle
+	{
+		public static readonly Proto proto = new Proto<M2Light>() {
+			id = "uv_m2lt",
+			level = 2,
+			type = Type.vehicle,
+			subtype = Subtype.light,
+			country = Country.usa,
+
+			power = 1,
+			defense = 0,
+			toughness = 3,
+			increase = 1,
+			cost = 1,
+		};
+		
+		public class UsaCamouflage : Camouflage
+		{
+			public override bool IsActive ()
+			{
+				return chief.cards.HasHq(Country.usa);
+			}
+		}
+
+		public override Proto GetProto ()
+		{
+			return proto;
+		}
+
+		public override void InitAbilities ()
+		{
+			base.InitAbilities();
+
+			abilities.Add(new UsaCamouflage());
+		}
+	}
+}
---
+++ Tests/Instances/Vehicles/M2LightTest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Midnight.Abilities.Passive;
+using Midnight.Cards.Enums;
+using Midnight.Cards.Types;
+using Midnight.ChiefOperations;
+using Midnight.Core;
+using Midnight.Instances.Germany.Orders;
+using Midnight.Instances.Germany.Vehicle;
+using Midnight.Instances.Usa.Vehicles;
+using Midnight.Tests.TestInstances;
+using Midnight.Utils;
+
+namespace Midnight.Tests.Instances.Vehicles
+{
+	[TestClass]
+	public class M2LightTest
+	{
+
+
+		[TestMethod]
+		public void InUsa ()
+		{
+			CheckSupply<HqGuards>(Status.TargetIsUnderCamouflage);
+		}
+
+		[TestMethod]
+		public void InGermany ()
+		{
+			CheckSupply<HqStrike>(Status.Success);
+		}
+
+		private void CheckSupply<TCard> (Status attackStatus)
+			where TCard : Hq, new()
+		{
+			Engine engine = new Engine();
+			Logger logger = new Logger(engine);
+			Manage manage = new Manage(engine);
+
+			var field = engine.field;
+			var player = engine.chiefs[0];
+			var enemy = engine.chiefs[1];
+
+			manage.SetResources(player, 50);
+
+			var myHq = player.cards.factory.CreateDefaultHq<TCard>();
+			var m2lt = player.cards.factory.Create<M2Light>();
+
+			var hisHq = enemy.cards.factory.CreateDefaultHq<HqStrike>();
+			var spg = enemy.cards.factory.Create<TankSpg>();
+
+			manage.Position(m2lt, field.GetCell(1, 1));
+			manage.Position(spg, field.GetCell(2, 1));
+
+			enemy.io.StartGame();
+
+			Assert.AreEqual(attackStatus, enemy.io.Attack(new Io.Target() {
+				sourceId = spg.id,
+				targetId = m2lt.id,
+			}));
+		}
+
+	}
+}
